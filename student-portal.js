294d041b4bedf32997ded3c4a34888b5180f6af8
eduproassist <eduproassist44@gmail.com>
2025-12-13 15:40:28 +0200

    Update student-portal.js

--- student-portal.js
+++ student-portal.js
@@ -1,6 +1,6 @@
 
 import { auth, db } from './firebase-config.js';
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 import {
   collection, query, where, getDocs, addDoc, doc, getDoc, setDoc, updateDoc, deleteDoc, orderBy, limit, onSnapshot
 } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
